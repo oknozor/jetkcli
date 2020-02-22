0d462c9013b0b12ca681644b77106f222812dacb
Paul Delafosse <paul.delafosse@protonmail.com>
2020-02-22 23:01:11 +0100

    chore(fmt): add TODO report on fmt

--- jetlib/src/settings/internal.rs
+++ jetlib/src/settings/internal.rs
@@ -6,7 +6,8 @@ pub struct InternalSettings {
 }
 
 // On the first transition transition id is retrieved from the http API
-// After that we store ids in the internal config file to avoid unecessary http calls
+// After that we store ids in the internal config file to avoid unecessary http
+// calls
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Workflow {
     pub wip: i32,
